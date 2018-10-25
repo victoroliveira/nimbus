1d1b219a76669cb46336dfef0997592e60895822
Victor Andrade de Oliveira <victoroliveira.eng@gmail.com>
2018-10-25 23:41:18 +0200

    Added settings file

---
+++ src/app/app.settings.ts
@@ -0,0 +1,38 @@
+export class AppSettings {
+  public static API_KEY = '84487947ce18e6d9bad1405df35f91df';
+  public static ENDPOINT = 'https://api.openweathermap.org/data/2.5/';
+  public static CITIES = [
+  {
+    id: 3448439,
+    name: 'Sao Paulo',
+    country: 'BR',
+    'coord': {
+      'lon': -46.636108,
+      'lat': -23.547501
+    }
+  }, {
+    id: 3435910,
+    name: 'Buenos Aires',
+    country: 'AR',
+    coord: {
+      lon: -58.377232,
+      lat: -34.613152
+    }
+  }, {
+    id: 3936456,
+    name: 'Lima',
+    country: 'PE',
+    coord: {
+      lon: -77.028236,
+      lat: -12.04318
+    }
+  }, {
+    id: 3871336,
+    name: 'Santiago',
+    country: 'CL',
+    coord: {
+      lon: -70.64827,
+      lat: -33.45694
+    }
+  }];
+}
