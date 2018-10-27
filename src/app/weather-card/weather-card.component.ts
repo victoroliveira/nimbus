05f831cdfac21113252aa706ce1ad3033f934bdc
Victor Andrade de Oliveira <victoroliveira.eng@gmail.com>
2018-10-27 14:21:08 +0200

    Show city weather information and icon

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AppSettings } from '../app.settings';
 
 @Component({
   selector: 'app-weather-card',
@@ -6,11 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./weather-card.component.scss']
 })
 export class WeatherCardComponent implements OnInit {
-  @Input() city: string;
+  @Input() city: Object;
 
   constructor() { }
 
+  private mountIconUrl(id: string) {
+    return `${AppSettings.ICON_ENDPOINT}${id}.png`;
+  }
+
+  private firstLetterUpper(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   ngOnInit() {
+    this.city.iconUrl = this.mountIconUrl(this.city.weather[0].icon);
+    this.city.description = this.firstLetterUpper(this.city.weather[0].description);
   }
 
 }
