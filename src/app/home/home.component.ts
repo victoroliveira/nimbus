b29c61930d2e447b8d4dd79ae08eb3cc1a80331d
Victor Andrade de Oliveira <victoroliveira.eng@gmail.com>
2018-10-27 16:24:12 +0200

    Unsubscribe weather service call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
 import { AppSettings } from '../app.settings';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +11,23 @@ import { AppSettings } from '../app.settings';
 export class HomeComponent implements OnInit {
   cities: any[] = new Array();
   citiesData: any;
+  weatherSub: Subscription;
 
   constructor(private weather: WeatherService) {
   }
 
   ngOnInit() {
     this.cities = AppSettings.CITIES;
-    this.weather.getCitiesWeatherById(this.cities)
+    this.weatherSub = this.weather.getCitiesWeatherById(this.cities)
     .subscribe((response) => {
       this.citiesData = response;
     }, (error) => {
       console.log(error);
     });
+  }
+
+  ngOnDestroy() {
+    this.weatherSub.unsubscribe();
   }
 
 }
